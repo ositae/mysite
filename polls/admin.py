a2706f2254438ee0766bf4350d0339bc690792bc
Your Name <you@example.com>
2024-02-10 17:39:31 -0500

    fix: psycopg2 fix, polls and question get route functional

--- polls/admin.py
+++ polls/admin.py
@@ -4,4 +4,4 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 admin.site.register(Question)
-admine.site.register(Choice)
+admin.site.register(Choice)

f8eb41d06b66b871a06ba1a3d1a65ca991ecc7b9
Your Name <you@example.com>
2024-02-10 18:31:05 -0500

    feat: update test.py file

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,7 +18,7 @@ import dj_database_url
 import psycopg2
 
 DATABASE_URL = os.getenv('DATABASE_URL')
-print(DATABASE_URL)
+# print(DATABASE_URL)
 conn = psycopg2.connect(DATABASE_URL, sslmode='require', port=5432)
 
 

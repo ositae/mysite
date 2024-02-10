83a8fbabe3891ca95744fcdff9c130603b4d1403
Your Name <you@example.com>
2024-02-10 18:13:05 -0500

    feat: POST route for polls question

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,11 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 # from pymongo import MongoClient
 # import os
 from django.template import loader
-from datetime import datetime, date, timedelta
+from django.utils import timezone
 from .models import Question, Choice
 # from dotenv import load_dotenv
 # load_dotenv()
@@ -109,15 +109,22 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five publised questions"""
-        return Question.objects.order_by("-pub_date")[:5]
-
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+    
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """ 
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
